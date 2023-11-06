63725551506cb1a9a5d24c3b29f8d9a1992ce5ba
Arafate3 <kankpaarafate3@gmail.com>
2023-11-06 13:28:52 +0000

    Mon premier commit

---
+++ src/main/java/com/Arafate3/App/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.Arafate3.App;
+
+/**
+ *
+ * @author Mr
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
